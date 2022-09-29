397197d631d58c22a83b9e08d4349b9c840a0d7e
nivaldoandrade <nivaldoandradef@gmail.com>
2022-09-29 17:49:47 -0300

    feat: creating global styles, default theme properties and applying ThemeProvider in the app

---
+++ src/assets/styles/global.js
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+
+	* {
+		margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+	}
+
+  @media (max-witdh: 1080px) {
+    html {
+      font-size: 93.75%; //16px
+    }
+  }
+
+  @media (max-width: 720opx) {
+    html {
+      font-size: 87.5%; //15px
+    }
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.backgroundColor};
+  }
+
+  body, input, button {
+    font: 700 1rem 'Sora', sans-serif;
+    color: black;
+  }
+
+  button  {
+    cursor: pointer;
+  }
+
+`;
