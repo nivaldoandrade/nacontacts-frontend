21b703f441a684f3cee3a3e6454302fe9bfea80c
nivaldoandrade <nivaldoandradef@gmail.com>
2022-10-07 13:04:54 -0300

    feat: creating contact list

--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -1,4 +1,8 @@
-import { Container, Header } from './styles';
+import { Container, Header, Divider, ListContainer, Card } from './styles';
+
+import arrowIcon from '../../assets/icons/arrow.svg';
+import editIcon from '../../assets/icons/edit.svg';
+import deleteIcon from '../../assets/icons/delete.svg';
 
 export function ContactList() {
   return (
@@ -7,6 +11,72 @@ export function ContactList() {
         <strong>3 contatos</strong>
         <a href="/">Novo Contato</a>
       </Header>
+
+      <Divider />
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrowIcon} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="info-header">
+              <strong>Mateus Silva</strong>
+              <small>instagram</small>
+            </div>
+            <span>mateus@devacademy.com.br</span>
+            <span>(41) 99999-9999</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={editIcon} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={deleteIcon} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="info-header">
+              <strong>Mateus Silva</strong>
+              <small>instagram</small>
+            </div>
+            <span>mateus@devacademy.com.br</span>
+            <span>(41) 99999-9999</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={editIcon} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={deleteIcon} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="info-header">
+              <strong>Mateus Silva</strong>
+              <small>instagram</small>
+            </div>
+            <span>mateus@devacademy.com.br</span>
+            <span>(41) 99999-9999</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={editIcon} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={deleteIcon} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
     </Container>
   );
 }
--- src/components/ContactsList/styles.js
+++ src/components/ContactsList/styles.js
@@ -12,7 +12,6 @@ export const Header = styled.header`
   strong {
     font-size: 24px;
     font-weight: 700;
-    color: #222222;
   }
 
   a {
@@ -30,6 +29,103 @@ export const Header = styled.header`
       color: #fff;
       background-color: ${({ theme }) => theme.colors.primary.main};
       border: 2px solid transparent;
+    }
+  }
+`;
+
+export const Divider = styled.div`
+  width: 100%;
+  height: 2px;
+  background-color: ${({ theme }) => theme.colors.gray[200]};
+  opacity: 0.2;
+  border-radius: 1px;
+  margin: 16px 0;
+`;
+
+export const ListContainer = styled.div`
+  header {
+    margin-bottom: 8px;
+
+    button {
+      background-color: transparent;
+      border: 0;
+      display: flex;
+      align-items: center;
+
+      span {
+        font-weight: 700;
+        color: ${({ theme }) => theme.colors.primary.main};
+      }
+
+      img {
+        margin-left: 8px;
+      }
+    }
+  }
+`;
+
+export const Card = styled.div`
+  width: 100%;
+  background-color: #fff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  border-radius: 4px;
+  padding: 16px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & + & {
+    margin-top: 16px;
+  }
+
+  &:last-child {
+    margin-bottom: 16px;
+  }
+
+  .info {
+    .info-header {
+      display: flex;
+      align-items: center;
+      margin-bottom: 4px;
+
+      strong {
+        color: #222;
+      }
+
+      small {
+        background-color: ${({ theme }) => theme.colors.primary.lighter};
+        border-radius: 4px;
+        margin-left: 8px;
+        padding: 4px;
+
+        font-size: 12px;
+        font-weight: 700;
+        color: ${({ theme }) => theme.colors.primary.main};
+        text-transform: uppercase;
+      }
+    }
+
+    span {
+      display: block;
+      font-size: 14px;
+      color: ${({ theme }) => theme.colors.gray[200]};
+      line-height: 18px;
+
+      & + span {
+        margin-top: 4px;
+      }
+    }
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+
+    button {
+      background-color: transparent;
+      border: 0;
+      margin-left: 8px;
     }
   }
 `;
