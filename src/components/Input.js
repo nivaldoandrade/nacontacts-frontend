2abe153c803cdcfee6dde7bde96ef9590a115a52
nivaldoandrade <nivaldoandradef@gmail.com>
2022-10-10 17:54:30 -0300

    feat: creating Select and Input components

---
+++ src/components/Input.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export default styled.input`
+  width: 100%;
+  height: 52px;
+  padding: 0 16px;
+  border: 0;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  border-radius: 4px;
+  background-color: #fff;
+  border: 2px solid transparent;
+  transition: border-color 0.2s ease-in;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray[200]};
+  }
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.primary.main};
+  }
+`;
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,5 +1,17 @@
+import Input from '../../components/Input';
 import { PageHeader } from '../../components/PageHeader';
+import Select from '../../components/Select';
 
 export function NewContact() {
-  return <PageHeader title="Novo contato" />;
+  return (
+    <>
+      <PageHeader title="Novo contato" />
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="1">Instagram</option>
+        <option value="2">Twitter</option>
+        <option value="3">Facebook</option>
+      </Select>
+    </>
+  );
 }
