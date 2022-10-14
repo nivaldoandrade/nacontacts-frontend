35b13ca88f32e432557d01a0cf7f584311c76c9d
nivaldoandrade <nivaldoandradef@gmail.com>
2022-10-14 18:06:22 -0300

    feat: getting the data and handling the submit form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 
+import { useState } from 'react';
 import { FormGroup } from '../FormGroup';
 import Input from '../Input';
 import Select from '../Select';
@@ -8,25 +9,57 @@ import Button from '../Button';
 import { Form, ButtonContainer } from './styles';
 
 export function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [telephone, setTelephone] = useState('');
+  const [category, setCategory] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log({
+      name,
+      email,
+      telephone,
+      category
+    });
+  }
+
   return (
-    <Form>
+    <Form onSubmit={(e) => handleSubmit(e)}>
       <FormGroup>
-        <Input placeholder="Nome" />
-      </FormGroup>
-      <FormGroup error="O formato do e-mail é inválido.">
-        <Input placeholder="E-mail" error />
-      </FormGroup>
-      <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          placeholder="Nome"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </FormGroup>
       <FormGroup>
-        <Select>
+        <Input
+          placeholder="E-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Input
+          placeholder="Telefone"
+          value={telephone}
+          onChange={(e) => setTelephone(e.target.value)}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="" disabled>
+            Categorias
+          </option>
           <option value="instagram">Instagram</option>
+          <option value="twitter">Twitter</option>
         </Select>
       </FormGroup>
 
       <ButtonContainer>
-        <Button type="button">{buttonLabel}</Button>
+        <Button>{buttonLabel}</Button>
       </ButtonContainer>
     </Form>
   );
