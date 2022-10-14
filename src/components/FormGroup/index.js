743c88b1b0c941573a7c45257fba18b0194f741d
nivaldoandrade <nivaldoandradef@gmail.com>
2022-10-14 12:58:35 -0300

    style: Error status in FormGroup and Input component

--- src/components/FormGroup/index.js
+++ src/components/FormGroup/index.js
@@ -1,10 +1,20 @@
 import PropTypes from 'prop-types';
 import { Container } from './styles';
 
-export function FormGroup({ children }) {
-  return <Container>{children}</Container>;
+export function FormGroup({ children, error }) {
+  return (
+    <Container>
+      {children}
+      {error && <small>{error}</small>}
+    </Container>
+  );
 }
 
 FormGroup.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  error: PropTypes.string
+};
+
+FormGroup.defaultProps = {
+  error: null
 };
--- src/components/FormGroup/styles.js
+++ src/components/FormGroup/styles.js
@@ -4,4 +4,11 @@ export const Container = styled.div`
   & + & {
     margin-top: 1rem;
   }
+
+  small {
+    display: block;
+    font-size: 12px;
+    color: ${({ theme }) => theme.colors.danger.main};
+    margin-top: 8px;
+  }
 `;
