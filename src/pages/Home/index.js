5f0626b691170ccebc734c46036880271ebb5bc0
nivaldoandrade <nivaldoandradef@gmail.com>
2022-10-20 23:28:01 -0300

    feat: rendering contact data returned from API on homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,7 @@
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+import formatPhone from '../../utils/formatPhone';
 
 import arrowIcon from '../../assets/icons/arrow.svg';
 import editIcon from '../../assets/icons/edit.svg';
@@ -14,6 +17,18 @@ import {
 } from './styles';
 
 export function Home() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/contacts')
+      .then(async (response) => {
+        const result = await response.json();
+
+        setContacts(result);
+      })
+      .catch((error) => console.log('FETCH CONTACTS', error));
+  }, []);
+
   return (
     <Container>
       <InputSearchBarContainer>
@@ -21,7 +36,9 @@ export function Home() {
       </InputSearchBarContainer>
 
       <ListHeader>
-        <strong>3 contatos</strong>
+        <strong>
+          {contacts.length} {contacts.length === 1 ? 'contato' : 'contatos'}
+        </strong>
         <Link to="/new">Novo Contato</Link>
       </ListHeader>
 
@@ -35,24 +52,28 @@ export function Home() {
           </button>
         </header>
 
-        <Card>
-          <div className="info">
-            <div className="info-header">
-              <strong>Mateus Silva</strong>
-              <small>instagram</small>
+        {contacts.map((contact) => (
+          <Card key={contact.id}>
+            <div className="info">
+              <div className="info-header">
+                <strong>{contact.name}</strong>
+                {contact.category_name && (
+                  <small>{contact.category_name}</small>
+                )}
+              </div>
+              <span>{contact.email}</span>
+              <span>{formatPhone(contact.phone)}</span>
             </div>
-            <span>mateus@devacademy.com.br</span>
-            <span>(41) 99999-9999</span>
-          </div>
-          <div className="actions">
-            <Link to="/edit/1">
-              <img src={editIcon} alt="Edit" />
-            </Link>
-            <button type="button">
-              <img src={deleteIcon} alt="Delete" />
-            </button>
-          </div>
-        </Card>
+            <div className="actions">
+              <Link to={`/edit/${contact.id}`}>
+                <img src={editIcon} alt="Edit" />
+              </Link>
+              <button type="button">
+                <img src={deleteIcon} alt="Delete" />
+              </button>
+            </div>
+          </Card>
+        ))}
       </ListContainer>
     </Container>
   );
