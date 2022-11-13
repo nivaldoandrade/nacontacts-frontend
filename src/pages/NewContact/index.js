ac31ecac33aa43d02563f985cc666d39c6bbb851
nivaldoandrade <nivaldoandradef@gmail.com>
2022-11-13 12:33:51 -0300

    refactor: creating useNewContact hook to abstract logic on newContact page

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,25 +1,10 @@
-import { useRef } from 'react';
-import ContactsService from '../../services/ContactsService';
+import { useNewContact } from './useNewContact';
 
 import { ContactForm } from '../../components/ContactForm';
 import { PageHeader } from '../../components/PageHeader';
 
-import toast from '../../utils/toast';
-
 export function NewContact() {
-  const contactFormRef = useRef(null);
-
-  async function handleSubmit(contact) {
-    try {
-      await ContactsService.createContact(contact);
-
-      contactFormRef.current.resetFields();
-
-      toast({ type: 'success', text: 'Contato cadastrado com sucesso!' });
-    } catch {
-      toast({ type: 'danger', text: 'Ocorreu um erro ao cadastra o contato!' });
-    }
-  }
+  const { contactFormRef, handleSubmit } = useNewContact();
   return (
     <>
       <PageHeader title="Novo contato" />
